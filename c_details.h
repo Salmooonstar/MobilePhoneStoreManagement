702ae6a1ca76b1bb6336f8bafaf0c8318e6f96d8
Salmooonstar <136182051+Salmooonstar@users.noreply.github.com>
2023-06-21 13:33:09 +0600

    Add files via upload

---
+++ c_details.h
@@ -0,0 +1,14 @@
+
+void customerdtls() {
+    char name[20];
+    char acn[20];
+    char password[15];
+    printf("\nName: ");
+    scanf("%s", &name);
+    printf("Account Number: ");
+    scanf("%s", &acn);
+    printf("Password: ");
+    scanf("%s", &password);
+
+    printf("Your order is successful");
+}
