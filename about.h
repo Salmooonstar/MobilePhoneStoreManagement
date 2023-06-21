e976dedd8754056d0e7dd487b3ad7349775be36b
Salmooonstar <136182051+Salmooonstar@users.noreply.github.com>
2023-06-21 13:52:05 +0600

    Add files via upload

---
+++ about.h
@@ -0,0 +1,9 @@
+void aboutSamsung() {
+    printf("About Samsung:\n");
+    printf("Samsung is a multinational conglomerate known for its electronics and technology products.\n");
+    printf("The company was founded in 1938 by Lee Byung-chul and has since become one of the world's largest technology companies.\n");
+    printf("Samsung offers a wide range of products, including smartphones, tablets, smartwatches, TVs, home appliances, and more.\n");
+    printf("With a focus on innovation and quality, Samsung has gained a strong reputation for its cutting-edge technology and sleek designs.\n");
+    printf("For more information, visit the official Samsung website.\n");
+}
+
